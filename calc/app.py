6789614ef4ac89d09247b4a30a15a5f08a6d3125
Taylor Wagner <wagner.taylorashley@gmail.com>
2021-04-13 11:42:38 -0500

    flask intro exercises

--- calc/app.py
+++ calc/app.py
@@ -1,1 +1,63 @@
 # Put your app in here.
+
+from flask import Flask, request
+from operations import add, sub, mult, div
+
+app = Flask(__name__)
+
+@app.route("/add")
+def adding():
+    """Add a and b."""
+    
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = add(a, b)
+
+    return str(result)
+
+@app.route("/sub")
+def subtracting():
+    """Substract b from a."""
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = sub(a, b)
+
+    return str(result)
+
+@app.route("/mult")
+def multiplying():
+    """Multiply a and b."""
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = mult(a, b)
+
+    return str(result)
+
+@app.route("/div")
+def dividing():
+    """Divide a by b."""
+
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+    result = div(a, b)
+
+    return str(result)
+
+
+calculate = {
+    "add": add,
+    "sub": sub,
+    "mult": mult,
+    "div": div
+}
+
+@app.route('/math/<calculation>')
+def calculator(calculation):
+    a = int(request.args.get("a"))
+    b = int(request.args.get("b"))
+
+    result = calculate[calculation](a, b)
+
+    return str(result)
